a1f5aa54ffe36e6fde7f056e6669a695f0091a60
wiktormuller <biznes.wiktor@gmail.com>
2020-03-12 14:30:25 +0100

    Domain models

---
+++ Passenger.Api/Passenger.Core/Domain/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Passenger.Core.Domain
+{
+    public class User
+    {
+        public Guid Id { get; protected set; }
+        public string Email { get; protected set; }
+        public string Password { get; protected set; }
+        public string Salt { get; protected set; }
+        public string Username { get; protected set; }
+        public string FullName { get; protected set; }
+        public DateTime CreatedAt { get; protected set; }
+
+        protected User()
+        {
+        }
+
+        public User(string email, string username, string password, string salt)    //EMAIL VALIDATION, ETC.
+        {
+            CreatedAt = DateTime.UtcNow;
+            Id = Guid.NewGuid();
+            Email = email;
+            Username = username;
+            Password = password;
+            Salt = salt;
+        }
+    }
+}
---
+++ Passenger.Api/Passenger.Core/Domain/Vehicle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Passenger.Core.Domain
+{
+    public class Vehicle
+    {
+        public string Brand { get; protected set; }
+        public string Name { get; protected set; }
+        public int Seats { get; protected set; }
+
+    }
+}
